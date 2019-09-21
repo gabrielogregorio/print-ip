e221d3d3fcf2e6886ac30d57b00e82f9b55184aa
Gabriel Gregório da Silva <48621655+gabrielogregorio@users.noreply.github.com>
2019-09-21 06:32:38 -0300

    Add files via upload

--- printIP.py
+++ printIP.py
@@ -6,12 +6,13 @@ __email__        = 'gabriel.gregorio.1@outlook.com'
 __description__  = 'Analisa diversos IPS no LINUX (UBUNTU) e mostra quais estão respondendo.'
 __status__       = 'Development'
 __date__         = '14/09/2019'
-__last_update__  = '15/09/2019'
-__version__      = '0.1'
+__last_update__  = '21/09/2019'
+__version__      = '0.2'
 
 print('*********** carregando software ********')
 
 from os import system
+import os
 from os import remove
 from threading import Thread
 
@@ -21,6 +22,8 @@ global itens
 global x
 global andante
 global defina_ip
+global dict_ips
+dict_ips = {}
 
 itens = []
 andante = 0
@@ -29,18 +32,19 @@ def loc():
     global x
     global andante
     global defina_ip
+    global dict_ips
 
     andante += 1
     try:
         ip = defina_ip + str(x)
         print('testando.: {}\r'.format(ip),end='')
-        system('ping -c 2 {} > {}'.format(ip,ip))
 
-        a = open(ip,'r')
-        b = a.read()
-        a.close()
+        cmd = "ping -c4 " + ip
+        resposta = "".join(os.popen(cmd).readlines())
 
-        if 'ttl=' in b:
+        dict_ips[ip] = resposta
+
+        if 'ttl=' in dict_ips[ip]  :
             global itens
             itens.append(ip)
         remove(ip)
@@ -48,8 +52,6 @@ def loc():
         pass
     andante -= 1
 
-
-# programa
 defina_ip = str(input('Digite um ip alvo (192.168.0.) : '))
 a         = str(input('Começar testar a partir de (0) : '))
 b         = str(input('Testar até qual  ip (150)      : '))
@@ -63,7 +65,6 @@ print('\n*********** aguardando respostas *******')
 while andante != 0:
     print('ips encontrados: ({}) | restantes: ({}) \r'.format(len(itens),andante),end='')
 print('\n*********** respostas OK ***************')
-
 
 print('\n*********** ips encontrados ************')
 for g in itens:
